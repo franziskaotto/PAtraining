f37322296ab1d1f5c1e42715009e1c7b4932ddcb
Franziska Otto <franziska.otto@gmx.at>
2023-07-20 16:02:26 +0200

    started 2. task

--- client/public/script.js
+++ client/public/script.js
@@ -1,3 +1,55 @@
 import { artObjects } from '/artObjects.js';
 
 // write your code here
+let rootE = document.querySelector("body");
+rootE.id = "root"
+
+initializeUi(artObjects);
+
+function initializeUi(artObjects) {
+  //more than 2 media items of type video
+  
+  
+  let filteredList = filterForVideo(artObjects)
+
+
+
+  
+
+
+}
+
+function filterForVideo(artObjects) {
+
+  let typeVideo = []
+
+
+  artObjects.forEach(art => {
+    let mediaItems = art["mediaItems"]
+    
+    for (let mediaItem of mediaItems) {
+      let type = mediaItem.mediaType
+
+      if (type === "video" &&) {
+        typeVideo.push(art)
+      }
+    }
+
+    
+    
+  });
+  console.log(typeVideo)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
